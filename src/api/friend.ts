d17c788ba5f55a43def02d9585b02fc9fdd0ea86
S-Hristov <132335296+S-Hristov@users.noreply.github.com>
2025-07-31 00:45:09 +0300

    fix

--- src/api/friend.ts
+++ src/api/friend.ts
@@ -3,27 +3,27 @@ const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function sendFriendRequest(userId: string, token: string) {
   const resp = await axios.post(
-    `${API_BASE_URL}/api/friend-requests/${userId}`,
+    `${API_BASE_URL}/friend-requests/${userId}`,
     {},
     { headers: { Authorization: `Bearer ${token}` } }
   );
   return resp.data;
 }
 export async function getReceivedFriendRequests(token: string) {
-  const resp = await axios.get(`${API_BASE_URL}/api/friend-requests/received`, {
+  const resp = await axios.get(`${API_BASE_URL}/friend-requests/received`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return resp.data;
 }
 export async function getSentFriendRequests(token: string) {
-  const resp = await axios.get(`${API_BASE_URL}/api/friend-requests/sent`, {
+  const resp = await axios.get(`${API_BASE_URL}/friend-requests/sent`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return resp.data;
 }
 export async function acceptFriendRequest(requestId: string, token: string) {
   const resp = await axios.post(
-    `${API_BASE_URL}/api/friend-requests/${requestId}/accept`,
+    `${API_BASE_URL}/friend-requests/${requestId}/accept`,
     {},
     { headers: { Authorization: `Bearer ${token}` } }
   );
@@ -31,7 +31,7 @@ export async function acceptFriendRequest(requestId: string, token: string) {
 }
 export async function declineFriendRequest(requestId: string, token: string) {
   const resp = await axios.post(
-    `${API_BASE_URL}/api/friend-requests/${requestId}/decline`,
+    `${API_BASE_URL}/friend-requests/${requestId}/decline`,
     {},
     { headers: { Authorization: `Bearer ${token}` } }
   );
--- src/api/games.ts
+++ src/api/games.ts
@@ -3,7 +3,7 @@ const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Get all games
 export async function getAllGames(token: string) {
-  const resp = await axios.get(`${API_BASE_URL}/api/games`, {
+  const resp = await axios.get(`${API_BASE_URL}/games`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return resp.data;
